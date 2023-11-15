86708412fbe670903d0d3201b322495fcfb29755
Yurii Cerber <yuriicerber@gmail.com>
2023-11-15 23:04:53 +0000

    Znayomstvo

---
+++ src/app/(info)/about/layout.tsx
@@ -0,0 +1,11 @@
+const AboutLayout = ({children,}: { children: React.ReactNode }) => {
+    return (
+        <div>
+            <span>About Layout</span>
+            {children}
+        </div>
+    )
+}
+
+export default AboutLayout
+
